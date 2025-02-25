9c56daf1a3ec50025ed8d1c79447ae2cb78d123a
jharry444 <jharrison1@qa.com>
2025-02-25 16:10:05 +0000

    components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Input from "./components/Input";
+
 function App() {
 
   console.log("Hi from App!");
@@ -6,8 +8,18 @@ function App() {
     <>
       <h1>Hello, World!</h1>
       <p>Here's my example React Application!</p>
+      <section>
+        <h2>Components</h2>
+        <input className="myClass" />
+        <p>2 + 2 = {2 + 2}</p>
+        <Input />
+        <Input />
+        {/* comment */}
+        <Input />
+        <Input />
+      </section>
     </>
-  )
+  );
 }
 
-export default App;
+export default App

139430c48d057c1810a9d680c62d33b61ff1a09a
jharry444 <jharrison1@qa.com>
2025-02-25 15:14:56 +0000

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+  console.log("Hi from App!");
+
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <p>Here's my example React Application!</p>
+    </>
+  )
+}
+
+export default App;

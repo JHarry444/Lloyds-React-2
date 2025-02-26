8c8f7da184ad5dda7b5f6140f96f08af62023f43
jharry444 <jharrison1@qa.com>
2025-02-26 09:50:57 +0000

    props

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Input from "./components/Input";
+import Trainer from "./components/Trainer";
 
 function App() {
 
@@ -13,6 +14,13 @@ function App() {
         <input className="myClass" />
         <p>2 + 2 = {2 + 2}</p>
         <Input />
+      </section>
+      <section>
+        <h2>Props</h2>
+        <Trainer name="Jordan" age={30} specialty="Java" />
+        <Trainer name="Cameron" age={32} specialty="JS" />
+        <Trainer name="Piers" age={28} specialty="DevOps" />
+
       </section>
     </>
   );
---
+++ src/components/Trainer.tsx
@@ -0,0 +1,19 @@
+type TrainerProps = {
+    name: string,
+    age: number,
+    specialty: string
+}
+
+const Trainer = function ({ name, age, specialty }: TrainerProps) {
+    console.log("age + 2 =", age + 2);
+
+    return (
+        <>
+            <p>Name: {name}</p>
+            <p>Age: {age}</p>
+            <p>Specialty: {specialty}</p>
+        </>
+    )
+}
+
+export default Trainer;
